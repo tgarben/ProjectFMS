13c9ac72740971db5a0ee46df84f598fdc13a10c
Tyler Garben <tylergarben@gmail.com>
2021-10-26 11:42:33 -0700

    Progress after October 26th 2021

--- sketch.js
+++ sketch.js
@@ -16,7 +16,11 @@ var compYpaddle;
 var compPaddleWidth = 15;
 var compPaddleHeight = 100;
 
+var playerScore = 0;
+var compScore = 0;
+
 var started = false;
+
 
 
 function setup() {
@@ -50,6 +54,36 @@ function setup() {
         yBallChange *=-1;
     }
 
+
+    // FIXME: COLLISSION NOT WORKING
+    if ((xBall > playerXpaddle && xBall < playerXpaddle + playerPaddleWidth) && (yBall + (diameter/2) >= playerYpaddle)){
+        xBallChange *= -1;
+        // yBallChange *= -1;
+        print("playerCollision");
+    }
+
+    if ((xBall > compXpaddle && xBall < compXpaddle + compPaddleWidth) && (yBall + (diameter/2) >= compYpaddle)){
+        xBallChange *= -1;
+        // yBallChange *= -1;
+        print("CompCollision");
+    }
+
+    if (xBall > (playerXpaddle + 10)){
+        print("Comp Score");
+        compScore++;
+        rerack();
+    }
+
+    if (xBall < (compXpaddle - 10)){
+        print("Player Score");
+        playerScore++;
+        rerack();
+    }
+
+    if (started){
+        compYpaddle = yBall - 50;
+    }
+
     if (!started){
         playerXpaddle = windowWidth - 75;
         // playerYpaddle = windowHeight / 2;
@@ -61,12 +95,52 @@ function setup() {
         started = true;
     }
 
+    playerYpaddle = winMouseY - 50;
+
+    if (playerYpaddle >= gameHeight - 100){
+        playerYpaddle = gameHeight - 100;
+    }
+    if (playerYpaddle <= 0){
+        playerYpaddle = 0;
+    }
+
+    if (compYpaddle >= gameHeight - 100){
+        compYpaddle = gameHeight - 100;
+    }
+    if (compYpaddle <= 0){
+        compYpaddle = 0;
+    }
+
     fill(0,255,255);
     noStroke();
-    rect(playerXpaddle, winMouseY - 50,playerPaddleWidth,playerPaddleHeight);
+    rect(playerXpaddle,playerYpaddle ,playerPaddleWidth,playerPaddleHeight);
     rect(compXpaddle, compYpaddle,compPaddleWidth,compPaddleHeight);
+
+    fill(0,255,255);
+    textSize(24);
+    text("Opponent Score: " + compScore, 10,25);
+
+    fill(33,33,33);
+    textSize(24);
+    text("Player Score: " + playerScore, windowWidth-195,25);
+
+    // print(yBall);
 
   }
   function backToMain(){
       window.location.href = "../index.html"
+  }
+
+  function rerack(){
+      xBall = windowWidth/2;
+      yBall = 50
+      yBallChange *= 0;
+      xBallChange *= 0;
+      setTimeout(function (){
+        // yBallChange *= 1;
+        // xBallChange *= 1;
+
+        xBallChange = 5;
+        yBallChange = 5;
+      }, 3000);
   }
