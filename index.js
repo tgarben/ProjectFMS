d7f39db458b4f74b53b4e2248c84a94e8ee0d47b
Tyler Garben <tylergarben@gmail.com>
2021-11-23 09:52:11 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+
+// Find the right method, call on correct element
+function launchFullScreen(element) {
+    if(element.requestFullScreen) {
+      element.requestFullScreen();
+    } else if(element.mozRequestFullScreen) {
+      element.mozRequestFullScreen();
+    } else if(element.webkitRequestFullScreen) {
+      element.webkitRequestFullScreen();
+    }
+  }
+  
+  // Launch fullscreen for browsers that support it!
+  launchFullScreen(document.documentElement); // the whole page
+
+
+
+
 function popUpAction(){
     alert("Button to game\nWIP");
 }
